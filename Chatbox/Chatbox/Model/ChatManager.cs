ab789f29f2f08ed49aa18c577e86dd877a7e7a56
Boudin Ahmed <boudin.ahmed@canadacouncil.ca>
2024-07-24 13:22:07 -0400

    main app

---
+++ Chatbox/Chatbox/Model/ChatManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatbox.Model
+{
+    internal class ChatManager
+    {
+        public Dictionary<User, string> chatHistory { get; set; }
+
+        public ChatManager() { }
+
+        public void sendMessage(User user, string message)
+        {
+
+            chatHistory.Add(user, message);
+        }
+    }
+}
